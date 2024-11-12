953bd72b321bb1b6ef3ca1d31e52c9d0b140ddcb
Phelipe Tussolini <phelipe@taller.net.br>
2024-11-11 23:00:20 -0300

    chore: fix role from users on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ services.each do |service|
   times = rand(1..3)
 
   times.times do
-    user = Fabricate(:user, description: Faker::Lorem.sentence)
+    user = Fabricate(:user, description: Faker::Lorem.sentence, role: :assistant)
     Fabricate(:user_service, price: rand(100..300), user: user, service: service)
   end
 end

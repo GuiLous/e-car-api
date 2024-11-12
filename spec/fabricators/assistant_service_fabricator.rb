cf6581ba25c3d9c78d34c2af58c4ea88e03aee85
GuiLouSilva <guilhermesilva@taller.net.br>
2024-11-12 15:31:44 -0300

    refactor: change table user service to assistant service

---
+++ spec/fabricators/assistant_service_fabricator.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: assistant_services
+#
+#  id           :bigint           not null, primary key
+#  price        :integer          default(0), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  assistant_id :bigint
+#  service_id   :bigint           not null
+#
+# Indexes
+#
+#  index_assistant_services_on_assistant_id  (assistant_id)
+#  index_assistant_services_on_service_id    (service_id)
+#
+# Foreign Keys
+#
+#  fk_rails_5c08927e25  (service_id => services.id)
+#  fk_rails_ff3182149c  (assistant_id => assistants.id)
+#
+Fabricator(:assistant_service) do
+  price { 0 }
+  service { Fabricate :service }
+  assistant { Fabricate :assistant }
+end

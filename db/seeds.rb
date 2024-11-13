c5a155ccefd7801db584a248eaa90725a7ea6030
Phelipe Tussolini <phelipe@taller.net.br>
2024-11-13 20:15:35 -0300

    refactor: change seeds to help on tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
+user = Fabricate :user
+user.wallet.add_coins(1000)
+
 services = []
 services << Fabricate(:service, name: 'League of Legends')
 services << Fabricate(:service, name: 'Counter Strike')
 services << Fabricate(:service, name: 'Valorant')
 
 services.each do |service|
-  times = rand(1..3)
-
-  times.times do
-    assistant = Fabricate(:assistant)
-    Fabricate(:assistant_service, price: rand(100..300), assistant: assistant, service: service)
-  end
+  assistant = Fabricate(:assistant)
+  Fabricate(:assistant_service, price: rand(100..300), assistant: assistant, service: service)
 end

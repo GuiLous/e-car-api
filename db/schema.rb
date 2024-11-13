260986386b4e6d905d0784078bde22de1dd86e17
GuiLouSilva <guilhermesilva@taller.net.br>
2024-11-13 18:01:26 -0300

    feat: add sidekiq and redis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_12_194242) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_13_150006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_12_194242) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "analysis_started_at"
     t.index ["assistant_service_id"], name: "index_hired_services_on_assistant_service_id"
     t.index ["user_id"], name: "index_hired_services_on_user_id"
   end

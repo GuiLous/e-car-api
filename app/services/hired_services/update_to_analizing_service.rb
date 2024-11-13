95b87454cff76bd7b8c7d4792a69982956eac914
GuiLouSilva <guilhermesilva@taller.net.br>
2024-11-13 18:06:38 -0300

    feat: create update to analizing service

---
+++ app/services/hired_services/update_to_analizing_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module HiredServices
+  class UpdateToAnalizingService
+    include Singleton
+
+    def update_to_analizing(hired_service_id:)
+      hired_service = HiredService.find(hired_service_id)
+
+      hired_service.update(status: :analyzing)
+
+      { message: "SUCCESS" }
+    end
+  end
+end

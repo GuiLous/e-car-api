a1ee8057d31761ff19a8b780934603d2adf7b98b
GuiLouSilva <guilhermesilva@taller.net.br>
2024-11-14 20:32:19 -0300

    chore: create jwt after sign_up

--- app/graphql/mutations/auth_mutations/sign_up.rb
+++ app/graphql/mutations/auth_mutations/sign_up.rb
@@ -3,20 +3,16 @@
 module Mutations
   module AuthMutations
     class SignUp < BaseMutation
-      include ::Authenticatable
-
       argument :email, String
       argument :name, String
       argument :password, String
       argument :password_confirmation, String
       argument :role, String, required: false, default_value: "member"
 
-      field :user, Types::UserType, null: true
+      field :token, String, null: true
 
       def resolve(email:, password:, password_confirmation:, role:, name:)
-        authenticate_user!
-
-        result = UserServices::CreatorService.instance.create(
+        UserServices::CreatorService.instance.create(
           email: email,
           password: password,
           name: name,
@@ -24,7 +20,7 @@ module Mutations
           role: role
         )
 
-        { user: result }
+        AuthServices::SignInService.instance.sign_in(email: email, password: password)
       end
     end
   end
--- app/services/auth_services/sign_in_service.rb
+++ app/services/auth_services/sign_in_service.rb
@@ -5,22 +5,11 @@ module AuthServices
     include Singleton
 
     def sign_in(email:, password:)
-      puts "\n\n----------------------"
-      puts email, password
-      puts "\n\n----------------------"
       user = User.find_for_database_authentication(email: email)
 
       raise Exceptions::InvalidCredentialsError unless user&.valid_password?(password)
 
-      puts "\n\n----------------------"
-      puts "user 2222 #{user.inspect}"
-      puts "\n\n----------------------"
-
       token = user.generate_jwt
-
-      puts "\n\n----------------------"
-      puts "token #{token}"
-      puts "\n\n----------------------"
 
       { token: token }
     end

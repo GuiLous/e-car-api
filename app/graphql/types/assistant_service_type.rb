078195318f8835f370fd45fd2622ec2d6fc8f961
Allan Carlos <allan@taller.net.br>
2024-11-22 19:31:06 -0300

    chore: return image url

--- app/graphql/types/assistant_service_type.rb
+++ app/graphql/types/assistant_service_type.rb
@@ -4,12 +4,17 @@ module Types
   class AssistantServiceType < Types::BaseObject
     field :assistant, Types::AssistantType, null: false
     field :id, ID, null: false
+    field :image_url, String, null: true
     field :price, Integer, null: false
     field :service, Types::ServiceType, null: false
     field :visible, Boolean, null: false
 
     def visible
       object.visible == "visible"
+    end
+
+    def image_url
+      ServiceCategory.find(object.service_category_id).image_url
     end
   end
 end

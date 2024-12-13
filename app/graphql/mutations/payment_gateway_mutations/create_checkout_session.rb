9369a9dab36d5ca6816b2ce19da504744e2c7999
GuiLouSilva <guilhermesilva@taller.net.br>
2024-12-13 09:09:13 -0300

    feat: create mutation create checkout session

---
+++ app/graphql/mutations/payment_gateway_mutations/create_checkout_session.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Mutations
+  module PaymentGatewayMutations
+    class CreateCheckoutSession < BaseMutation
+      include ::Authenticatable
+
+      argument :price, Float, required: true
+      argument :price_id, String, required: true
+
+      field :session_id, String, null: false
+      field :session_url, String, null: false
+
+      def resolve(price:, price_id:)
+        authenticate_user!
+
+        PaymentGatewayServices::CreateCheckoutSessionService.instance.create_checkout_session(
+          price: price,
+          price_id: price_id,
+          current_user: context[:current_user]
+        )
+      rescue StandardError => e
+        Rails.logger.error e.message
+        raise GraphQL::ExecutionError, "SYSTEM_ERROR"
+      end
+    end
+  end
+end
